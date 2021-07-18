76acf2647892d231a22551b5e56110b22f44e5f6
mamontinnokent <61653414+mamontinnokent@users.noreply.github.com>
2021-07-18 14:20:13 +0300

    message update

--- src/main/java/ru/chat/controller/MessageController.java
+++ src/main/java/ru/chat/controller/MessageController.java
@@ -3,8 +3,10 @@ package ru.chat.controller;
 
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import ru.chat.service.MessageService;
+
+import java.security.Principal;
 
 @Controller
 @AllArgsConstructor
@@ -14,5 +16,27 @@ public class MessageController {
     private final MessageService messageService;
 
 
+    @PostMapping("create")
+    public String create(ChatDTO chatDTO, Principal principal) {
+        // ! some code
+        return "";
+    }
+    @DeleteMapping("delete/{id}")
+    public String delete(@RequestParam Long id) {
+        // ! some code
+        return "";
+    }
+
+    @GetMapping("update/{id}")
+    public String update(@RequestParam Long id) {
+        // ! some code
+        return "";
+    }
+
+    @GetMapping("{id}")
+    public String get(@RequestParam Long id) {
+        // ! some code
+        return "";
+    }
 
 }
