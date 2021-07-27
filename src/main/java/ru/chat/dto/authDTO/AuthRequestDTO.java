8beaaf6f55c64bf8b42b37889ae35f4d30694c8b
Денис Зингер <61653414+mamontinnokent@users.noreply.github.com>
2021-07-27 19:38:08 +0300

    /

--- src/main/java/ru/chat/dto/authDTO/AuthRequestDTO.java
+++
@@ -1,9 +0,0 @@
-package ru.chat.dto.authDTO;
-
-import lombok.Data;
-
-@Data
-public class AuthRequestDTO {
-    private String email;
-    private String password;
-}
--- src/main/java/ru/chat/dto/userDTO/UpdateUserDTO.java
+++
@@ -1,9 +0,0 @@
-package ru.chat.dto.userDTO;
-
-import lombok.Data;
-
-@Data
-public class UpdateUserDTO {
-    private String username;
-    private String email;
-}
